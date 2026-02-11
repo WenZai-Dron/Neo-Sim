c0a2a09ce4e8e2fdd685c052bc8f3e74535e88ad
Dron <zzw15815821081@163.com>
2026-02-11 21:59:35 +0800

    构建
    0.0.2

--- src/main/java/com/wenzai/neosim/NeoSimClient.java
+++ src/main/java/com/wenzai/neosim/NeoSimClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = NeoSim.MODID, dist = Dist.CLIENT)
+@Mod(value = NeoSim.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = NeoSim.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = NeoSim.MOD_ID, value = Dist.CLIENT)
 public class NeoSimClient {
     public NeoSimClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
