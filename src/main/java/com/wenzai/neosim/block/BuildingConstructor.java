77c58e88e23612c96f4cbc5565334ef676e6f4e7
Dron <zzw15815821081@163.com>
2026-03-06 23:29:15 +0800

    0.2.3

---
+++ src/main/java/com/wenzai/neosim/block/BuildingConstructor.java
@@ -0,0 +1,11 @@
+package com.wenzai.neosim.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class BuildingConstructor extends Block
+{
+    public BuildingConstructor(Properties properties)
+    {
+        super(properties);
+    }
+}
