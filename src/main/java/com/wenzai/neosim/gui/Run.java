5c56dbfe9ba587f6ea1fee1f1c9f286e15352ee5
Dron <zzw15815821081@163.com>
2026-05-09 14:15:45 +0800

    UPDATE

--- src/main/java/com/wenzai/neosim/gui/Run.java
+++ src/main/java/com/wenzai/neosim/gui/Run.java
@@ -1,5 +1,6 @@
 package com.wenzai.neosim.gui;
 
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.components.Button;
@@ -13,10 +14,8 @@ public class Run extends Screen
     }
 
     public short[] Mode = new short[1];
-    public short[] SingeOrMuti = new short[1];
-
-    private Button button1, button2, button3, button4, button5, button6;
-
+    public short[] SingleOrMulti = new short[1];
+    Button Button1, Button2, Button3, Button4, Button5, Button6;
     private final Component tip1 = Component.translatable("gui.neosim.run.tip1"),
                             tip2 = Component.translatable("gui.neosim.run.tip2"),
                             tip3 = Component.translatable("gui.neosim.run.tip3");
@@ -30,74 +29,82 @@ public class Run extends Screen
     @Override
     protected void init()
     {
-        this.button1 = new Button.Builder(Component.translatable("gui.neosim.run.button1"), Button -> {
+        Button1 = Button.builder(Component.translatable("gui.neosim.run.button1"), Button -> {
             Mode[0] = 1;
-            onClose();
+            Button2.active = true;
+            Button3.active = true;
+            Button.active = false;
         })
                 .pos(90, 85)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button1);
 
-        this.button2 = new Button.Builder(Component.translatable("gui.neosim.run.button2"), Button -> {
+        Button2 = Button.builder(Component.translatable("gui.neosim.run.button2"), Button -> {
             Mode[0] = 2;
-            onClose();
+            Button1.active = true;
+            Button3.active = true;
+            Button.active = false;
         })
                 .pos(90, 145)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button2);
 
-        this.button3 = new Button.Builder(Component.translatable("gui.neosim.run.button3"), Button -> {
+        Button3 = Button.builder(Component.translatable("gui.neosim.run.button3"), Button -> {
             Mode[0] = 3;
-            onClose();
+            Button1.active = true;
+            Button2.active = true;
+            Button.active = false;
         })
                 .pos(90, 205)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button3);
 
-        this.button4 = new Button.Builder(Component.translatable("gui.neosim.run.button4"), Button -> {
-            SingeOrMuti[0] = 1;
-            onClose();
+        Button4 = Button.builder(Component.translatable("gui.neosim.run.button4"), Button -> {
+            SingleOrMulti[0] = 1;
+            Button5.active = true;
+            Button.active = false;
         })
                 .pos(265, 85)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button4);
 
-        this.button5 = new Button.Builder(Component.translatable("gui.neosim.run.button5"), Button -> {
-            SingeOrMuti[0] = 2;
-            onClose();
+        Button5 = Button.builder(Component.translatable("gui.neosim.run.button5"), Button -> {
+            SingleOrMulti[0] = 2;
+            Button4.active = true;
+            Button.active = false;
         })
                 .pos(265, 145)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button5);
 
-        this.button6 = new Button.Builder(Component.translatable("gui.neosim.run.button6"), Button -> {
+        Button6 = Button.builder(Component.translatable("gui.neosim.run.button6"), Button -> {
             onClose();
         })
                 .pos(265, 205)
                 .size(135, 20)
                 .build();
+        this.addRenderableWidget(Button6);
     }
 
     // 渲染组件
     @Override
     public void render(GuiGraphics GuiGraphics, int MouseX, int MouseY, float PartialTick)
     {
-        this.renderBackground(GuiGraphics, MouseX, MouseY, PartialTick);
-
         // 渲染背景
         this.renderBackground(GuiGraphics, MouseX, MouseY, PartialTick);
 
-        GuiGraphics.drawCenteredString(this.font, tip1, 100, 110, 0xFFFFFF00);
-        GuiGraphics.drawCenteredString(this.font, tip2, 100, 170, 0xFFFFFF00);
-        GuiGraphics.drawCenteredString(this.font, tip3, 100, 230, 0xFFFFFF00);
-
-        this.button1.render(GuiGraphics, MouseX, MouseY, PartialTick);
-        this.button2.render(GuiGraphics, MouseX, MouseY, PartialTick);
-        this.button3.render(GuiGraphics, MouseX, MouseY, PartialTick);
-        this.button4.render(GuiGraphics, MouseX, MouseY, PartialTick);
-        this.button5.render(GuiGraphics, MouseX, MouseY, PartialTick);
-        this.button6.render(GuiGraphics, MouseX, MouseY, PartialTick);
-
+        // 调用父类渲染，即渲染按钮
         super.render(GuiGraphics, MouseX, MouseY, PartialTick);
+
+        Font font = this.font;
+
+        GuiGraphics.drawCenteredString(font, tip1, 150, 110, 0xFFFFFF00);
+        GuiGraphics.drawCenteredString(font, tip2, 150, 170, 0xFFFFFF00);
+        GuiGraphics.drawCenteredString(font, tip3, 150, 230, 0xFFFFFF00);
     }
 }
